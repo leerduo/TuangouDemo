b93739489310e16fd1bae1385dfb92402405fd1f
leerduo <eoeduo@163.com>
2016-11-09 21:36:19 +0800

    SplashActivity

--- app/src/main/java/cn/chenfuduo/tuangoudemo/SplashActivity.java
+++ app/src/main/java/cn/chenfuduo/tuangoudemo/SplashActivity.java
@@ -1,13 +1,39 @@
 package cn.chenfuduo.tuangoudemo;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+    private boolean isFirst = true;
+    private SharedPreferences sp;
+    private String isFisrtTime = "isFirst";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        sp = getSharedPreferences(isFisrtTime, MODE_PRIVATE);
+        isFirst = sp.getBoolean(isFisrtTime, true);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                if (isFirst) {
+                    intent.setClass(SplashActivity.this, GuideActivity.class);
+                    isFirst = false;
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putBoolean(isFisrtTime, SplashActivity.this.isFirst);
+                    editor.commit();
+                } else {
+                    intent.setClass(SplashActivity.this, MainActivity.class);
+                }
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
